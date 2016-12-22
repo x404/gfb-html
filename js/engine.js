3ec7e8625637c74b768571dd414def0a3607ff21
x404 <kronos2003@gmail.com>
2016-12-22 11:52:41 +0200

    Сделаны модальные окна и валидация форм

--- js/engine.js
+++ js/engine.js
@@ -1,3 +1,45 @@
 $(document).ready(function(){
-	$('#fullpage').fullpage();	
+	$('#fullpage').fullpage();
+
+	// вызов полной формы регистрации
+	$('button[data-target="#registration"]').click(function(e){
+		e.preventDefault;
+		$('#registration .email').show();
+		$('#registration').modal('show');
+	});
+
+	// вызов укороченной формы регистрации
+	$('#plate_form .submit').click(function(e){
+		e.preventDefault;
+		$(this).closest('form').submit();
+	});
+
+
+	// VALIDATE
+	// форма входа
+	$('#auth_form').validate({
+		submitHandler: function(form){
+			// при прохождении валидации, дальнейший код тут (ajax)
+			console.log("Валидация пройдена");
+		}
+	});
+
+	// плашка на первом слайдере
+	$('#plate_form').validate({
+		submitHandler: function(form){
+			// скрываем поле с email
+			$('#registration .email').val($('#plate_form .email').val()).hide();
+
+			// показываем модальное окно регистрации
+			$('#registration').modal('show');
+		}
+	});
+	
+	// форма регистрации
+	$('#registration_form').validate({
+		submitHandler: function(form){
+			// при прохождении валидации, дальнейший код тут
+			console.log("Валидация пройдена");
+		}
+	});
 })
